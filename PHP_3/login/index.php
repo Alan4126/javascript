1849d6b7181dc3ec730168122fbe4d6745bcaf5f
Alan <jehe_18@alu.uabcs.mx>
2020-11-24 20:33:03 -0700

    24/11/2020

---
+++ PHP_3/login/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		Login
+	</title>
+</head>
+<body>
+	
+			
+		<fieldset>
+			<legend>
+				Access in your account
+			</legend>
+			<label>
+				Email
+			</label>
+			<input type="email" name="email" required="">
+			<br>
+			<label>
+				Password
+			</label>
+			<input type="passsord" name="passsord" required="">
+			<br>
+			<button type="submit">
+				Access
+			</button>
+		</fieldset>
+	
+</body>
+</html> 
